eecb42606b140cd03739b1c03ab0fe0ecec1b6d0
Жугинский Иван <zhuga200117@gmail.com>
2022-05-16 21:03:42 +0300

    feat: :sparkles: Написал микросервис отправки сообщений

---
+++ messaging-service/src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require('cors');
+const TelegramBot = require('node-telegram-bot-api');
+const { sendChatMessage, getChatMessages } = require("./messaging");
+const { dbClientInit } = require("./dbAPI");
+
+require('dotenv').config();
+
+const app = express();
+
+const token = process.env.BOT_API_KEY;
+
+const bot = new TelegramBot(token);
+
+app.use(cors());
+app.use(express.json());
+
+//#region Messages
+app.post("/sendMessage", (request, response) => {
+    sendChatMessage(request.body.userName, request.body.messageBody, bot).then(
+        () => response.sendStatus(200)
+    );
+})
+
+app.get("/getMessages", (request, response) => {
+    getChatMessages().then(
+        (messages) => response.send(messages)
+    );
+})
+//#endregion
+
+dbClientInit(app, 3001, process.env.MONGO_URL);
